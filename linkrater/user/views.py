eea8412f5ade1e7c890400582fdaccac3be98729
Pranav.PL <pranavpl.tvm@gmail.com>
2024-09-09 14:45:51 +0530

    first commit

--- linkrater/user/views.py
+++ linkrater/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
+# Create your views here
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=password1)
+            login(request,user)
+
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
+    return render(request,'user/signup.html',{
+        'form':form
+        })
